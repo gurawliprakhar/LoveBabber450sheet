2763924a3ebacd1d2c294d01e5f415fe3dd3921c
Prakhar Tripathi <110407222+gurawliprakhar@users.noreply.github.com>
2023-11-02 18:42:16 +0530

    completed the code:

--- src/Array/Reversearray.java
+++ src/Array/Reversearray.java
@@ -1,10 +1,42 @@
-package Array;
+package Array;//no issue in using but use lowercase to follow convention
 
-//reverse an array
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+//Ques 1- reverse an array
+
 public class Reversearray {
 
-	public static void main(String[] args) {
+	static void reverseArray(int[] ar) {
 		
+		for (int i = 0; i < ar.length/2; i++) {
+			int temp = ar[i];
+			ar[i] = ar[ar.length-1-i];
+			ar[ar.length-1-i] = temp; 
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int[] ar = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			ar[i] = scan.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.err.print(ar[i] + " ");
+		}
+		reverseArray(ar);
+		
+		System.out.println();
+		
+		for(int i=0; i<n; i++) {
+			System.err.print(ar[i] + " ");
+		}
 
 	}
 
